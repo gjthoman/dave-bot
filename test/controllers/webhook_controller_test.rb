9552f4e85ad3614a22993228a07c0e19ff99ab6c
Greg Thoman <greg.thoman@daveramsey.com>
2017-03-23 13:51:37 -0500

    remove DB

--- test/controllers/webhook_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class WebhookControllerTest < ActionDispatch::IntegrationTest
-  test "should get submit" do
-    get webhook_submit_url
-    assert_response :success
-  end
-
-end
