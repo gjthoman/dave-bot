8d681287faeef4e78fa3f1866776e9165f0a0997
Greg Thoman <greg.thoman@daveramsey.com>
2017-03-23 14:04:50 -0500

    basic how_much_house

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,6 +1,11 @@
 class WebhookController < ApplicationController
   def submit
-    speech = "API is working"
+    #binding.pry
+    speech = "Sorry, try to ask your question a different way"
+
+    if params[:result][:action] == "how_much_house"
+      speech = how_much_house
+    end
 
     msg = {
       "speech": speech,
@@ -12,5 +17,11 @@ class WebhookController < ApplicationController
 
       format.json { render json: msg }
     end
+  end
+
+  def how_much_house
+    amount = params[:result][:parameters][:amount]
+
+    "Dave recommends your housing payment be less than one quarter of your take home pay. With an annual income of $#{amount}, you can safely spend up to $#{amount.to_i/12/4} per month on housing."
   end
 end
